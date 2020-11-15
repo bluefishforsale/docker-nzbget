4713d154450c5343ee37ab03faff2dbcbb0f492c
terrac <terracnosaur@gmail.com>
2020-11-15 17:02:29 +0000

    latest update

--- build-docker.sh
+++ build-docker.sh
@@ -4,4 +4,4 @@ BUILD=latest
 SERVICE=nzbget
 USER=bluefishforsale
 
-docker build --pull --tag ${USER}/${SERVICE}:${BUILD} .
+docker build --pull --tag ${USER}/${SERVICE}:${BUILD} . && docker push ${USER}/${SERVICE}:${BUILD}
--- nzbget.sh
+++ nzbget.sh
@@ -4,7 +4,6 @@ SERVICE="nzbget"
 IMAGE="bluefishforsale/nzbget"
 VERSION="latest"
 LOCALDIR="/data01/services/${SERVICE}"
-WORKDIR="/M2scratch/${SERVICE}"
 DOWNLOADS="/data01/incoming"
 MOVIES="/data01/complete/movies"
 TV="/data01/complete/tv"
@@ -12,15 +11,18 @@ TV="/data01/complete/tv"
 docker stop ${SERVICE}
 docker rm ${SERVICE}
 
+docker images | egrep "${IMAGE}\ *${VERSION}" | awk '{print $3}' | xargs docker rmi
+
 
 sudo docker run -d \
   --restart=always \
+  --cpu-shares=600 \
   --name=${SERVICE} \
   --hostname=${HOSTNAME} \
   -e PUID=1001 -e PGID=100\
   -p 8901:8901 \
-  -v ${LOCALDIR}:/config \
-  -v ${WORKDIR}:/scratch \
+  -v ${LOCALDIR}/config:/config \
+  -v ${LOCALDIR}/scratch:/scratch \
   -v ${MOVIES}:/movies \
   -v ${TV}:/tv \
   -v ${DOWNLOADS}:/downloads \
